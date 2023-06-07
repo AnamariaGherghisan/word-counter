e01493b7f80c18ccd7d60d98600948e2e0810145
Anamaria Gherghisan <sanduanamaria44@yahoo.com>
2023-06-07 22:47:10 +0100

    add logic to counter

--- src/components/WordCounter.js
+++ src/components/WordCounter.js
@@ -1,8 +1,27 @@
+import { useState } from "react";
+
 export const WordCounter = () => {
-  return <div>
+  const [wordCount, setWordCount] = useState(0)
+
+  const handleOnChange = (event) => {
+    //get words from  textarea
+    const value = event.target.value.trim();
+    console.log(value);
+    //count the words
+    const words = value.split(" ");
+    console.log(words)
+    //get word length
+    const wordLength = words.length;
+    console.log(wordLength);
+    //set the count in state
+    setWordCount(wordLength);
+  };
+  return (
+  <div>
     <div>
-      <textarea rows={10} placeholder="Type or paste your text here"></textarea>
+      <textarea rows={10} placeholder="Type or paste your text here" onChange={handleOnChange}></textarea>
     </div>
-    <div>Word count: 0</div>
-    </div>;
+    <div>Word count: {wordCount}</div>
+    </div>
+  );
 };
