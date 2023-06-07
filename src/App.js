e9b1d938cb727cf55eff8e37057bc531ae06dfcf
Anamaria Gherghisan <sanduanamaria44@yahoo.com>
2023-06-07 22:11:44 +0100

    complete WordCounter

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import React from 'react';
+import { Header } from './components/Header';
+import {WordCounter} from "./components/WordCounter";
+
 export  const App = () => {
-  return <div>App</div>; 
+  return (
+  <div>
+    <Header title="Word Counter" subTitle="Paste or type your text here"/>
+    <WordCounter/>
+  </div>
+  );
 };
---
+++ src/components/WordCounter.js
@@ -0,0 +1,8 @@
+export const WordCounter = () => {
+  return <div>
+    <div>
+      <textarea rows={10} placeholder="Type or paste your text here"></textarea>
+    </div>
+    <div>Word count: 0</div>
+    </div>;
+};
